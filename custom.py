5f3f77c7ec3e703c1a500fe17fbdcb38b57eb5ac
jsch <joe.sch@protonmail.com>
2022-10-28 16:43:34 +0200

    remove even more cuda stuff; implement annotating feature on custom images

--- custom.py
+++ custom.py
@@ -24,24 +24,27 @@ def annotate_image(first_image: np.ndarray) -> np.ndarray:
     fig, ax = plt.subplots()
     ax.imshow(first_image)
 
-    points = []
+    xs, ys = [], []
 
     def onclick(event):
         #print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
         #  ('double' if event.dblclick else 'single', event.button,
         #   event.x, event.y, event.xdata, event.ydata))
-        points.append([event.xdata, event.ydata])
+        xs.append(event.xdata)
+        ys.append(event.ydata)
         ax.scatter(x=[event.xdata], y=[event.ydata], color='red')
         fig.canvas.draw()
 
     cid = fig.canvas.mpl_connect('button_press_event', onclick)
     plt.show()
     
-    return np.array(points)
+    return xs, ys
 
 
 def run_model(model, rgbs, N, sw):
-    #rgbs = rgbs.cuda().float() # B, S, C, H, W
+    first_image = rgbs[0][0].permute(1, 2, 0).numpy()
+    xs, ys = annotate_image(first_image)
+
     rgbs = rgbs.float() # B, S, C, H, W
 
     B, S, C, H, W = rgbs.shape
@@ -51,63 +54,93 @@ def run_model(model, rgbs, N, sw):
     H, W = H_, W_
     rgbs = rgbs_.reshape(B, S, C, H, W)
 
-    # get annoations by the user
-    im = imageio.imread(filenames[0]).astype(np.uint8)
-    annotate_image(im)
-
-    # pick N points to track; we'll use a uniform grid
-    N_ = np.sqrt(N).round().astype(np.int32)
-    grid_y, grid_x = utils.basic.meshgrid2d(B, N_, N_, stack=False, norm=False, device='cpu')
-    grid_y = 8 + grid_y.reshape(B, -1)/float(N_-1) * (H-16)
-    grid_x = 8 + grid_x.reshape(B, -1)/float(N_-1) * (W-16)
-    xy = torch.stack([grid_x, grid_y], dim=-1) # B, N_*N_, 2
+    # try to pick a point on the dog, so we get an interesting trajectory
+    # x = torch.randint(-10, 10, size=(1, N), device=torch.device('cuda')) + 468
+    # y = torch.randint(-10, 10, size=(1, N), device=torch.device('cuda')) + 118
+    #x = torch.ones((1, N), device=torch.device('cpu')) * 450.0
+    #y = torch.ones((1, N), device=torch.device('cpu')) * 100.0
+    x = torch.tensor([xs], device=torch.device('cpu'), dtype=torch.float32)
+    y = torch.tensor([ys], device=torch.device('cpu'), dtype=torch.float32)
+    xy0 = torch.stack([x, y], dim=-1) # B, N, 2
     _, S, C, H, W = rgbs.shape
 
-    print_stats('rgbs', rgbs)
-    preds, preds_anim, vis_e, stats = model(xy, rgbs, iters=6)
-    trajs_e = preds[-1]
-    print_stats('trajs_e', trajs_e)
+    trajs_e = torch.zeros((B, S, N, 2), dtype=torch.float32, device='cpu')
+    for n in range(N):
+        # print('working on keypoint %d/%d' % (n+1, N))
+        cur_frame = 0
+        done = False
+        traj_e = torch.zeros((B, S, 2), dtype=torch.float32, device='cpu')
+        traj_e[:,0] = xy0[:,n] # B, 1, 2  # set first position 
+        feat_init = None
+        while not done:
+            end_frame = cur_frame + 8
+
+            rgb_seq = rgbs[:,cur_frame:end_frame]
+            S_local = rgb_seq.shape[1]
+            rgb_seq = torch.cat([rgb_seq, rgb_seq[:,-1].unsqueeze(1).repeat(1,8-S_local,1,1,1)], dim=1)
+
+            outs = model(traj_e[:,cur_frame].reshape(1, -1, 2), rgb_seq, iters=6, feat_init=feat_init, return_feat=True)
+            preds = outs[0]
+            vis = outs[2] # B, S, 1
+            feat_init = outs[3]
+            
+            vis = torch.sigmoid(vis) # visibility confidence
+            xys = preds[-1].reshape(1, 8, 2)
+            traj_e[:,cur_frame:end_frame] = xys[:,:S_local]
+
+            found_skip = False
+            thr = 0.9
+            si_last = 8-1 # last frame we are willing to take
+            si_earliest = 1 # earliest frame we are willing to take
+            si = si_last
+            while not found_skip:
+                if vis[0,si] > thr:
+                    found_skip = True
+                else:
+                    si -= 1
+                if si == si_earliest:
+                    # print('decreasing thresh')
+                    thr -= 0.02
+                    si = si_last
+            # print('found skip at frame %d, where we have' % si, vis[0,si].detach().item())
+
+            cur_frame = cur_frame + si
+
+            if cur_frame >= S:
+                done = True
+        trajs_e[:,:,n] = traj_e
     
     pad = 50
     rgbs = F.pad(rgbs.reshape(B*S, 3, H, W), (pad, pad, pad, pad), 'constant', 0).reshape(B, S, 3, H+pad*2, W+pad*2)
     trajs_e = trajs_e + pad
+
+    prep_rgbs = utils.improc.preprocess_color(rgbs)
+    gray_rgbs = torch.mean(prep_rgbs, dim=2, keepdim=True).repeat(1, 1, 3, 1, 1)
     
     if sw is not None and sw.save_this:
         linewidth = 2
 
-        # visualize the input
-        o1 = sw.summ_rgbs('inputs/rgbs', utils.improc.preprocess_color(rgbs[0:1]).unbind(1))
-        # visualize the trajs overlaid on the rgbs
-        o2 = sw.summ_traj2ds_on_rgbs('outputs/trajs_on_rgbs', trajs_e[0:1], utils.improc.preprocess_color(rgbs[0:1]), cmap='spring', linewidth=linewidth)
-        # visualize the trajs alone
-        o3 = sw.summ_traj2ds_on_rgbs('outputs/trajs_on_black', trajs_e[0:1], torch.ones_like(rgbs[0:1])*-0.5, cmap='spring', linewidth=linewidth)
-        # concat these for a synced wide vis
-        wide_cat = torch.cat([o1, o2, o3], dim=-1)
-        sw.summ_rgbs('outputs/wide_cat', wide_cat.unbind(1))
+        for n in range(N):
+            # print('visualizing kp %d' % n)
+            kp_vis = sw.summ_traj2ds_on_rgbs('video_%d/kp_%d_trajs_e_on_rgbs' % (sw.global_step, n), trajs_e[0:1,:,n:n+1], gray_rgbs[0:1,:S], cmap='spring', linewidth=linewidth)
 
-        # write to disk, in case that's more convenient
-        wide_list = list(wide_cat.unbind(1))
-        wide_list = [wide[0].permute(1,2,0).cpu().numpy() for wide in wide_list]
-        wide_list = [Image.fromarray(wide) for wide in wide_list]
-        out_fn = './out_%d.gif' % sw.global_step
-        wide_list[0].save(out_fn, save_all=True, append_images=wide_list[1:])
-        print('saved %s' % out_fn)
-
-        # alternate vis
-        sw.summ_traj2ds_on_rgbs2('outputs/trajs_on_rgbs2', trajs_e[0:1], vis_e[0:1], utils.improc.preprocess_color(rgbs[0:1]))
+            # write to disk, in case that's more convenient
+            kp_list = list(kp_vis.unbind(1))
+            kp_list = [kp[0].permute(1,2,0).cpu().numpy() for kp in kp_list]
+            kp_list = [Image.fromarray(kp) for kp in kp_list]
+            out_fn = './chain_out_%d.gif' % sw.global_step
+            kp_list[0].save(out_fn, save_all=True, append_images=kp_list[1:])
+            print('saved %s' % out_fn)
+            
+        sw.summ_traj2ds_on_rgb('outputs/trajs_e_on_rgb', trajs_e[0:1], prep_rgbs[0:1,0], cmap='spring')
+        sw.summ_traj2ds_on_rgb('outputs/trajs_e_on_rgb2', trajs_e[0:1], torch.mean(prep_rgbs[0:1], dim=1), cmap='spring')
         
-        # animation of inference iterations
-        rgb_vis = []
-        for trajs_e_ in preds_anim:
-            trajs_e_ = trajs_e_ + pad
-            rgb_vis.append(sw.summ_traj2ds_on_rgb('', trajs_e_[0:1], torch.mean(utils.improc.preprocess_color(rgbs[0:1]), dim=1), cmap='spring', linewidth=linewidth, only_return=True))
-        sw.summ_rgbs('outputs/animated_trajs_on_rgb', rgb_vis)
 
     return trajs_e-pad
-
+    
 def main():
 
-    # the idea in this file is to run the model on some demo images, and return some visualizations
+    # the idea in this file is to chain together pips from a long sequence, and return some visualizations
     
     exp_name = '00' # (exp_name is used for logging notes that correspond to different runs)
 
@@ -115,15 +148,15 @@ def main():
 
     ## choose hyps
     B = 1
-    S = 8
-    N = 16**2 # number of points to track
+    S = 50
+    N = 1 # number of points to track
 
-    filenames = glob.glob('./demo_images/*.jpg')
+    filenames = glob.glob('./custom_images/*.jpg')
     filenames = sorted(filenames)
     print('filenames', filenames)
-    max_iters = len(filenames)//S # run each unique subsequence
+    max_iters = len(filenames)//(S//2)-1 # run slightly overlapping subseqs
 
-    log_freq = 2 # when to produce visualizations 
+    log_freq = 1 # when to produce visualizations 
     
     ## autogen a name
     model_name = "%02d_%d_%d" % (B, S, N)
@@ -133,12 +166,12 @@ def main():
     model_name = model_name + '_' + model_date
     print('model_name', model_name)
     
-    log_dir = 'logs_demo'
+    log_dir = 'logs_chain_demo'
     writer_t = SummaryWriter(log_dir + '/' + model_name + '/t', max_queue=10, flush_secs=60)
 
     global_step = 0
 
-    model = Pips(stride=4) #.cuda()
+    model = Pips(stride=4)
     parameters = list(model.parameters())
     if init_dir:
         _ = saverloader.load(init_dir, model)
@@ -155,14 +188,14 @@ def main():
             writer=writer_t,
             global_step=global_step,
             log_freq=log_freq,
-            fps=5,
+            fps=12,
             scalar_freq=int(log_freq/2),
             just_gif=True)
 
         try:
             rgbs = []
             for s in range(S):
-                fn = filenames[(global_step-1)*S+s]
+                fn = filenames[(global_step-1)*S//2+s]
                 if s==0:
                     print('start frame', fn)
                 im = imageio.imread(fn)
--- saverloader.py
+++ saverloader.py
@@ -55,7 +55,7 @@ def load(ckpt_dir, model, optimizer=None, scheduler=None, model_ema=None, step=0
                 # 3. load the new state dict
                 model.load_state_dict(model_dict, strict=False)
             else:
-                checkpoint = torch.load(path)
+                checkpoint = torch.load(path, map_location='cpu')
                 model.load_state_dict(checkpoint['model_state_dict'], strict=False)
                 
             if optimizer is not None:
